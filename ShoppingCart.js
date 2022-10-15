b0d6cec7d5cacab5686d4793bc9904478855f47d
Nhatbao1 <91550513+Nhatbao1@users.noreply.github.com>
2022-10-15 14:12:39 +0700

    Initial commit

---
+++ ShoppingCart.js
@@ -0,0 +1,353 @@
+let products = [
+    {
+        "id": 1,
+        "name": "Brocolli - 1 Kg",
+        "price": 120,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 2,
+        "name": "Cauliflower - 1 Kg",
+        "price": 60,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 3,
+        "name": "Cucumber - 1 Kg",
+        "price": 48,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cucumber.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 4,
+        "name": "Beetroot - 1 Kg",
+        "price": 32,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beetroot.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 5,
+        "name": "Carrot - 1 Kg",
+        "price": 56,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/carrots.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 6,
+        "name": "Tomato - 1 Kg",
+        "price": 16,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/tomato.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 7,
+        "name": "Beans - 1 Kg",
+        "price": 82,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beans.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 8,
+        "name": "Brinjal - 1 Kg",
+        "price": 35,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/brinjal.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 9,
+        "name": "Capsicum",
+        "price": 60,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/capsicum.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 10,
+        "name": "Mushroom - 1 Kg",
+        "price": 75,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/button-mushroom.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 11,
+        "name": "Potato - 1 Kg",
+        "price": 22,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/potato.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 12,
+        "name": "Pumpkin - 1 Kg",
+        "price": 48,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pumpkin.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 13,
+        "name": "Corn - 1 Kg",
+        "price": 75,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/corn.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 14,
+        "name": "Onion - 1 Kg",
+        "price": 16,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/onion.jpg",
+        "category": "vegetables"
+    },
+    {
+        "id": 15,
+        "name": "Apple - 1 Kg",
+        "price": 72,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/apple.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 16,
+        "name": "Banana - 1 Kg",
+        "price": 45,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/banana.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 17,
+        "name": "Grapes - 1 Kg",
+        "price": 60,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/grapes.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 18,
+        "name": "Mango - 1 Kg",
+        "price": 75,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/mango.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 19,
+        "name": "Musk Melon - 1 Kg",
+        "price": 36,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/musk-melon.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 20,
+        "name": "Orange - 1 Kg",
+        "price": 75,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/orange.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 21,
+        "name": "Pears - 1 Kg",
+        "price": 69,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pears.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 22,
+        "name": "Pomegranate - 1 Kg",
+        "price": 95,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pomegranate.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 23,
+        "name": "Raspberry - 1/4 Kg",
+        "price": 160,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/raspberry.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 24,
+        "name": "Strawberry - 1/4 Kg",
+        "price": 180,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/strawberry.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 25,
+        "name": "Water Melon - 1 Kg",
+        "price": 28,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/water-melon.jpg",
+        "category": "fruits"
+    },
+    {
+        "id": 26,
+        "name": "Almonds - 1/4 Kg",
+        "price": 200,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/almonds.jpg",
+        "category": "nuts"
+    },
+    {
+        "id": 27,
+        "name": "Pista - 1/4 Kg",
+        "price": 190,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/pista.jpg",
+        "category": "nuts"
+    },
+    {
+        "id": 28,
+        "name": "Nuts Mixture - 1 Kg",
+        "price": 950,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/nuts-mixture.jpg",
+        "category": "nuts"
+    },
+    {
+        "id": 29,
+        "name": "Cashews - 1 Kg",
+        "price": 650,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/cashews.jpg",
+        "category": "nuts"
+    },
+    {
+        "id": 30,
+        "name": "Walnuts - 1/4 Kg",
+        "price": 170,
+        "image": "https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/walnuts.jpg",
+        "category": "nuts"
+    }
+]
+let newProducts = products.map(prod => ({ ...prod, quantity: 1 }))
+let cart = []
+const productEle = document.querySelector(".product-lists .row")
+const searchInputEle = document.querySelector(".search input")
+const searchButtonEle = document.querySelector(".search button")
+const numItemsEle = document.querySelector(".num-items")
+const totalEle = document.querySelector(".total")
+const cartDropDownEle = document.querySelector(".cart-dropdown")
+const renderProducts = (products) => {
+    let list = ""
+    products.forEach(prod => {
+        list += `   <div class="col">
+        <div class="item" data-id="${prod.id}">
+            <div class="img">
+                <img src="${prod.image}">
+            </div>
+            <h2 class="title">${prod.name}</h2>
+            <span class="price">${prod.price}$</span>
+            <div class="cart-quantity flex a-center j-center">
+                <button class=" btn btn-dec">-</button>
+                <input value="${prod.quantity}" class="input-quantity" type="number">
+                <button class="btn btn-inc">+</button>
+            </div>
+            <button class="btn-add">ADD TO CART</button>
+        </div>
+    </div>`
+    })
+    productEle.innerHTML = list
+}
+renderProducts(newProducts)
+searchInputEle.addEventListener("keyup", function (e) {
+    const searchValue = searchInputEle.value
+    const searchProducts = newProducts.filter((val) =>
+        val.name.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    renderProducts(searchProducts)
+})
+productEle.addEventListener("click", function (e) {
+    const target = e.target
+    const itemEle = target.closest(".item")
+    if (target.classList.contains("btn-inc")) {
+        const id = +itemEle.dataset.id
+        const itemIdx = newProducts.findIndex(prod => prod.id === id)
+        const item = newProducts[itemIdx]
+        item.quantity += 1
+        target.closest(".cart-quantity").children[1].value = item.quantity
+    }
+
+    if (target.classList.contains("btn-dec")) {
+        const id = +itemEle.dataset.id
+        const itemIdx = newProducts.findIndex(prod => prod.id === id)
+        const item = newProducts[itemIdx]
+        if (item.quantity === 1) {
+            return
+        } else {
+            item.quantity -= 1
+
+        }
+        target.closest(".cart-quantity").children[1].value = item.quantity
+    }
+    if (target.classList.contains("btn-add")) {
+        const id = +itemEle.dataset.id
+        const itemIdx = newProducts.findIndex(prod => prod.id === id)
+        const item = newProducts[itemIdx]
+        const idx = cart.findIndex((val) => val.id === item.id)
+        if (idx === -1) {
+            cart.push({ ...item })
+        }
+        else {
+            cart[idx].quantity += item.quantity
+        }
+        renderCartInfo(cart)
+        renderCart(cart)
+    }
+    if (target.classList.contains("input-quantity")) {
+        const id = +itemEle.dataset.id
+        const itemIdx = newProducts.findIndex(prod => prod.id === id)
+        const item = newProducts[itemIdx]
+        target.addEventListener("keyup", function (e) {
+            item.quantity = + e.target.value
+        })
+    }
+})
+renderCartInfo = (cart) => {
+    numItemsEle.textContent = cart.length
+    const total = cart.reduce((acc, val) => {
+        return acc + val.price * val.quantity
+    }, 0)
+    totalEle.textContent = `$${total}`
+}
+const cartIconEle = document.querySelector(".cart-icon i")
+cartIconEle.addEventListener("click", function (e) {
+    const isDisplay = cartDropDownEle.style.display === "none" ? false : true
+    if (isDisplay) {
+        cartDropDownEle.style.display = "none"
+        return
+    }
+    cartDropDownEle.style.display = "block"
+
+})
+renderCart = (cart) => {
+    if (!cart.length) {
+        cartDropDownEle.innerHTML = `<div class="img">
+        <img src="empty-cart.png">
+        <p>Yourt cart is empty</p>
+        <button class="btn-checkout disabled">PROCEED TO CHECKOUT</button>
+    </div>`
+        return
+    }
+    let listCart = ""
+    cart.forEach(cart => {
+        listCart += `
+        <li data-id = ${cart.id}>
+            <img src="${cart.image}">
+            <span >${cart.price}$</span >
+            <span class="quantity-cart"> No: ${cart.quantity} </span>
+            <span class="cart-total">${cart.price * cart.quantity}$</span>
+            <span class="btn-delete"> X </span>
+        </li >
+            `
+    })
+    cartDropDownEle.innerHTML = `
+            <ul class="cart-item">
+                ${listCart}
+            </ul>
+        <button class="btn-checkout ">PROCEED TO CHECKOUT</button>
+             `
+}
+renderCart(cart)
+cartDropDownEle.addEventListener("click", function (e) {
+    const isDeletebtn = e.target.closest(".btn-delete")
+    if (isDeletebtn) {
+        const li = e.target.closest("li")
+        const id = +li.dataset.id
+        const productsDelete = cart.filter(val => val.id !== id)
+        cart = productsDelete
+        renderCart(cart)
+        renderCartInfo(cart)
+        return
+    }
+})
